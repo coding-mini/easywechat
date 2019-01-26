0b5c084a768cfe0aa6138c2ebc2ba47c3935d3d8
coding-mini <mini@coding10.com>
2019-01-26 20:39:33 +0800

    update

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -7,11 +7,12 @@ use Illuminate\Support\Facades\Log;
 
 class WechatController extends Controller
 {
-    public function server()
+    public function serve()
     {
         Log::info('I am wechat server');
 
         $app = app('wechat.official_account');
+
         $app->server->push(function($message){
             return "欢迎关注 coding10！";
         });
