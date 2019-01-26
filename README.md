37209f9b0688fc72bca066ed91dfaa75e14d254b
Codinget <codinget@163.com>
2019-01-26 11:00:39 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,1 @@
+#微信公众后开发 development
---
+++ app/Http/Controllers/WechatController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class WechatController extends Controller
+{
+    public function server()
+    {
+        Log::info('I am wechat server');
+
+        $app = app('wechat.official_account');
+        $app->server->push(function($message){
+            return "欢迎关注 coding10！";
+        });
+
+        return $app->server->serve();
+    }
+}
